932ee5ab9bd86eb0828b5155d55f246f0e3705d8
nassneg <snailfail@yandex.ru>
2020-05-04 22:34:38 +0300

    Draft Planing

---
+++ scriptInclude.js
@@ -0,0 +1,19 @@
+// Tesseract Js Script
+
+function onChange(control, oldValue, newValue, isLoading, isTemplate) {
+if (isLoading || newValue === '') {
+return;
+}
+
+ScriptLoader.getScripts('ocr.jsdbx', function() {
+Tesseract.recognize('https://dev82471.service-now.com/' + g_form.getValue('name'), newValue, {
+logger: function logger(m) {
+return g_form.setValue('u_ocr_text', 'Progress: ' + ((parseFloat(m['progress']) * 100).toFixed(2)) + '%');
+}
+}).then(function(_ref) {
+var text = _ref.data.text;
+g_form.setValue('u_ocr_text', text);
+});
+});
+
+}
