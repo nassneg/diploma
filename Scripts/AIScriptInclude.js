2b1312e39e71e3465700b7af9d8622847bd279ef
nassneg <snailfail@yandex.ru>
2020-05-18 16:47:41 +0300

    BR

--- Scripts/AIScriptInclude.js
+++ Scripts/AIScriptInclude.js
@@ -38,7 +38,47 @@ GoogleAIRest.prototype = Object.extendsObject(AbstractAjaxProcessor, {
 });
 
 //business rule
-var base = new GoogleAIRest();
+(function executeRule(current, previous /*null when async*/ ) {
 
-var str = base.base64Encode('98e56702db70101018d02fb748961906');
-base.requestJSON(str);
+	var currID = '';
+	currID += current.sys_id;
+	gs.addInfoMessage(currID);
+    var base = new GoogleAIRest();
+    var str = base.base64Encode(currID);
+    var req = base.requestJSON(str);
+
+
+    try {
+        var restMessage = new sn_ws.RESTMessageV2();
+        restMessage.setHttpMethod("POST");
+        restMessage.setEndpoint("https://vision.googleapis.com/v1/images:annotate?key=AIzaSyBHnApG-d0uXm8sImwYkY6jp75P8Uy8i2k");
+        restMessage.setBasicAuth("", "");
+
+
+        restMessage.setRequestBody(req);
+        var response = restMessage.execute();
+        if (response.getStatusCode() === 200) {
+            var responseBody = response.getBody();
+            var responseData = JSON.parse(responseBody);
+			var text = responseData.description;
+			gs.addInfoMessage("Description" + responseData);
+            gs.addInfoMessage("Good! " + responseBody);
+            // process successful response
+        } else if (response.getStatusCode() === 0) {
+			gs.addInfoMessage("Good! " + responseBody);
+            // process connect timed out
+        } else {
+            // process error response
+            var statusCode = response.getStatusCode();
+            var erorMessage = response.getErrorMessage();
+            var contentType = response.getHeader("Content-Type");
+            var body = response.getBody();
+			gs.addInfoMessage("NOT Good! " + body);
+            // ...
+        }
+    } catch (ex) {
+        // process exception
+    }
+
+
+})(current, previous);
